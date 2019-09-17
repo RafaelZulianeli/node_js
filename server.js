4544b3928f2a30b6c9654c3ae6fb0a45d5da15ae
RafaelZulianeli <rafael.zulianeli@divitech.com.br>
2019-09-16 22:13:34 -0300

    Verbos http

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.get('/', (req, res) => {
     res.status(200).send('Olá Mundão!')
 })
 
+app.post('/', (req, res) => {
+    res.status(201).send('Criado')
+})
+
+app.put('/', (req, res) => {
+    res.status(202).send('Atualizado')
+})
+
+app.delete('', (req, res) => {
+    res.status(204).send('Excluído')
+})
+
 app.listen(3000, () => {
     console.log('Servidor API iniciado na porta 3000.')
 })
