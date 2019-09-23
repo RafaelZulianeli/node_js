7ffd10bbd32401d8cf1413ce38f1b00925d1e9fe
RafaelZulianeli <rafael.zulianeli@divitech.com.br>
2019-09-23 20:00:34 -0300

    Atualiza express

--- src/bin/configuration/variables.js
+++ src/bin/configuration/variables.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database: {
+        connection: process.env.connection || 'mongodb+srv://ionic_3_admin:Kthw4hQzURzR2dLn@cluster0-yko7h.mongodb.net/test?retryWrites=true&w=majority'
     }
 }
 
--- src/bin/express.js
+++ src/bin/express.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const variables = require('./configuration/variables')
 
 const categoriaRouter = require('../routes/categoria.route')
 const produtoRouter = require('../routes/produto.route')
@@ -10,6 +12,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+
+mongoose.connect(variables.Database.connection)
 
 app.use('/api/categoria', categoriaRouter)
 app.use('/api/produto', produtoRouter)
